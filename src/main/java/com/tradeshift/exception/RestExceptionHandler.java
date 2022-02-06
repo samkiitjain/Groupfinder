a68342d783879e923ab9b0e7f34c565a6d949908
Samkit Jain <jainsamkit26@gmail.com>
2022-02-06 22:03:00 +0100

    Improved logging and enable h2 console.

--- src/main/java/com/tradeshift/exception/RestExceptionHandler.java
+++ src/main/java/com/tradeshift/exception/RestExceptionHandler.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler({Exception.class, NullPointerException.class, SQLException.class, HibernateException.class})
 	public ResponseEntity<Object> handleInternalError(Exception ex, WebRequest request){
+		System.out.println(ex);
 		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
 	}
 
 	@ExceptionHandler(InvalidInputException.class)
 	public ResponseEntity<Object> handleInvalidInputError(Exception ex, WebRequest request){
+		System.out.println(ex);
 		return handleExceptionInternal(ex, ex.getLocalizedMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 }
